c961555b6b35c88788322cb42812e34fc5d3dd08
kproj <k.yamamoto99@gmail.com>
2021-10-11 20:31:10 +0900

    10/11 change

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def handle_message(event):
     else:#"確認" または "チェック"以外のメッセージを入力した場合はオウム返し
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(event.message.text + text='はわかりません！'))
+            # TextSendMessage(event.message.text + text='はわかりません！'))
             TextSendMessage(text='"確認","test","写真"を入力してください'))
             messe = event.message.text
 
