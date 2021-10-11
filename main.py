4342f404f5f4f047a5807a487a9fccd16599a26d
kproj <k.yamamoto99@gmail.com>
2021-10-11 20:42:04 +0900

    10/11 change

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import os
 
+import os
 from argparse import ArgumentParser
 from logging.config import dictConfig
-
 from flask import Flask, request, abort
-
 from flask.logging import default_handler
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -17,6 +14,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
     UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent,
 )
+
 import wikipedia
 
 
@@ -38,14 +36,11 @@ dictConfig({
         'handlers': ['consoleHandler']
     }
 })
-
 app = Flask(__name__)
-
 # 環境変数取得
 # LINE Developersで設定されているチャネルアクセストークンとチャネルシークレットを設定
 YOUR_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_BOT_CHANNEL_TOKEN")
 YOUR_CHANNEL_SECRET = os.getenv("LINE_BOT_CHANNEL_SECRET")
-
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
@@ -54,17 +49,15 @@ wikipedia.set_lang("ja")
 
 
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
     """ Webhookからのリクエストの正当性をチェックし、ハンドラに応答処理を移譲する """
-
     # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
-
     # リクエストボディを取得します。
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     
     try:
@@ -76,46 +69,21 @@ def callback():
         print("署名検証で失敗してます" , YOUR_CHANNEL_ACCESS_TOKEN)
     # handleの処理を終えればOK
     return 'OK'
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """
     LINEへのテキストメッセージに対して応答を返す
-
     Parameters
     ----------
     event: MessageEvent
       LINEに送信されたメッセージイベント
     """
-
     if (event.reply_token == '00000000000000000000000000000000' or event.reply_token == 'ffffffffffffffffffffffffffffffff'):
         return
-    
 
-
-    #"確認" または "チェック"のメッセージを入力した場合"OK"のメッセージを送信して、
-    #撮影してからLINE Notifyで画像を送信
-    if messe == "確認" or messe == "チェック":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="OK"))
-   
-    elif messe == "test" or messe == "テスト":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="バッチリだよ！！"))
-    
-    
-
-
-    else:#"確認" または "チェック"以外のメッセージを入力した場合はオウム返し
-        line_bot_api.reply_message(
-            event.reply_token,
-            # TextSendMessage(event.message.text + text='はわかりません！'))
-            TextSendMessage(text='"確認","test","写真"を入力してください'))
-
-
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="応答です。 " + event.message.text))
 
 if __name__ == "__main__":
     # app.run()
@@ -128,5 +96,4 @@ if __name__ == "__main__":
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     arg_parser.add_argument('--host', default='0.0.0.0', help='host')
     options = arg_parser.parse_args()
-
     app.run(debug=options.debug, host=options.host, port=options.port)
