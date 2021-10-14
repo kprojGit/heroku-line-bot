5885345207345fc1607c7cb9974c2337c58e0d81
kproj <k.yamamoto99@gmail.com>
2021-10-15 03:52:22 +0900

    20211014 add

--- janken.py
+++ janken.py
@@ -26,15 +26,14 @@ def janken_battle(messe):
     bot_answer = random.choice(['ぐー', 'ちょき', 'ぱー'])
 
     reply_messages = []
-    win_reply_message = [TextSendMessage(text='私の勝ちです')]
+    win_reply_message = [TextSendMessage(text='私の勝ちです!')]
     win_reply_message.append(StickerSendMessage(package_id='1', sticker_id=random.choice(['106', '407', '125', '100', '110'])))
-    win_reply_message.append(FlexSendMessage(alt_text='最初はぐー', contents=saisyohaguu_message))
-    lose_reply_message = [TextSendMessage(text='私の負けです')]
+    lose_reply_message = [TextSendMessage(text='私の負けです…')]
     lose_reply_message.append(StickerSendMessage(package_id='2', sticker_id=random.choice(['152', '18', '25', '173', '524'])))
-    lose_reply_message.append(FlexSendMessage(alt_text='最初はぐー', contents=saisyohaguu_message))
+    lose_reply_message = [TextSendMessage(text='あいこで…')]
     draw_reply_message = [FlexSendMessage(alt_text='あいこで', contents=saisyohaguu_message)]
 
-    
+
     if request_message == 'ぐー':
         reply_messages.append(TextSendMessage(text=bot_answer))
         if bot_answer == 'ぐー':
