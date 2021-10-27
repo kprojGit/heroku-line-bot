e88712c0ff37ac36886d42d65ad01593588e7656
kproj <k.yamamoto99@gmail.com>
2021-10-27 23:01:28 +0900

    20211027 add

--- main.py
+++ main.py
@@ -225,9 +225,9 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             ImageSendMessage(
-                original_content_url = FQDN + “/static/” + “test.jpg”,
-                preview_image_url = FQDN + “/static/” + “test.jpg”)
-                #FQDN + “/static/” + event.message.id + “jpg”
+                original_content_url = FQDN + '/static/' + 'test.jpg',
+                preview_image_url = FQDN + '/static/' + 'test.jpg')
+                #FQDN + '/static/' + event.message.id + 'jpg'
             
 
 
