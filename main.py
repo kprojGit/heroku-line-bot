db6e57911b131a79c94532ee6b0e04db09f51349
kproj <k.yamamoto99@gmail.com>
2021-11-09 00:29:24 +0900

    20211108 add

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ def handle_audio_message(event):
     FQDN = 'https://yama365.herokuapp.com'
     line_bot_api.reply_message(
         event.reply_token,
-        AudioSendMessage(original_content_url = FQDN + '/static/' + 'sample.m4a")
+        AudioSendMessage(original_content_url = FQDN + '/static/' + 'sample.m4a')
         )
 
 
