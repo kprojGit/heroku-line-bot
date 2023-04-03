337f0a0042db77873962b44721c776d32ed872dc
kproj <k.yamamoto99@gmail.com>
2023-04-04 03:41:59 +0900

    abort400 change

--- testChatGPT.py
+++ testChatGPT.py
@@ -1,12 +1,12 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "語頭には「あーはいはい、それね。」、すべての語尾に「らしいのぉ。」か「わい。」をつけて質問に短く答えてください"},
-        {"role": "user", "content": "APIってなに？"},
+        {"role": "user", "content": "この時期に関東で釣れる魚ってなに？"},
     ],
     max_tokens=150
 )
