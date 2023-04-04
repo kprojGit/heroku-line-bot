a3e0e1a7b58beab6873efd462e557085aa3a0d20
kproj <k.yamamoto99@gmail.com>
2023-04-05 04:31:15 +0900

    all change

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
-import os
-from flask import Flask, request, abort
-
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
-from linebot.exceptions import (LineBotApiError, InvalidSignatureError)
-
-
-
-
+from flask import Flask
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.getenv["LINE_BOT_CHANNEL_TOKEN"]
-YOUR_CHANNEL_SECRET = os.getenv["LINE_BOT_CHANNEL_SECRET"]
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return "OK"
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    try:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
-    except LineBotApiError as e:
-        print("LineBotApiError:", e.status_code, e.error.message)
-        abort(500)
-
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def hello_world():
+  return "Hello, World!"
