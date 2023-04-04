ffe4a02de55da77d896af19ebe44cd4aaf99aae5
kproj <k.yamamoto99@gmail.com>
2023-04-05 01:17:33 +0900

    gpt change

--- main.py
+++ main.py
@@ -1,58 +1,37 @@
 import os
-import json
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn)
 
 app = Flask(__name__)
 
-ABS_PATH = os.path.dirname(os.path.abspath(__file__))
-with open(ABS_PATH+'/conf.json', 'r') as f:
-    CONF_DATA = json.load(f)
-
-#LINEへのアクセス情報を入力
-YOUR_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_BOT_CHANNEL_TOKEN")
-YOUR_CHANNEL_SECRET = os.getenv("LINE_BOT_CHANNEL_SECRET")
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(os.environ["LINE_BOT_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["LINE_BOT_CHANNEL_SECRET"])
 
 
-
-#ルーティングの設定、POSTリクエストが来たらcallback関数を返す
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=["POST"])
 def callback():
-    # リクエストヘッダーからアクセス情報の検証のための値を取得
-    signature = request.headers['X-Line-Signature']
-
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # アクセス情報を検証し、成功であればhandleの関数を呼び出す
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-    return 'OK'
+    return "OK"
 
-#メッセージを受け取った後にどんな処理を行うかを記述
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    try:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+    except LineBotApiError as e:
+        print("LineBotApiError:", e.status_code, e.error.message)
+        abort(500)
 
-
-    if (event.reply_token == '00000000000000000000000000000000' or event.reply_token == 'ffffffffffffffffffffffffffffffff'):
-        return
-
-    elif line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
